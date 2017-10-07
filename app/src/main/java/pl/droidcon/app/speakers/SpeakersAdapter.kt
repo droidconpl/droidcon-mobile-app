2fca2b47a96faf0845fd7e6c66418702cabad149
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-10-07 19:53:19 +0200

    Adding photos of speakers to the list

--- app/src/main/java/pl/droidcon/app/speakers/SpeakersAdapter.kt
+++ app/src/main/java/pl/droidcon/app/speakers/SpeakersAdapter.kt
@@ -4,10 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import pl.droidcon.app.R
 import pl.droidcon.app.data.Speaker
 import pl.droidcon.app.ext.bind
+
+private const val BASE_IMAGE_URL = "https://raw.githubusercontent.com/droidconpl/droidcon-2016-web/master/assets/photos/speakers/"
 
 class SpeakersAdapter(private val speakers: List<Speaker>) : RecyclerView.Adapter<SpeakerViewHolder>() {
 
@@ -23,8 +27,10 @@ class SpeakersAdapter(private val speakers: List<Speaker>) : RecyclerView.Adapte
 class SpeakerViewHolder(item: View) : RecyclerView.ViewHolder(item) {
 
     private val name by item.bind<TextView>(R.id.speaker_name)
+    private val image by item.bind<ImageView>(R.id.speaker_picture)
 
     fun bindHolder(speaker: Speaker) {
         name.text = speaker.firstName
+        Picasso.with(image.context).load(BASE_IMAGE_URL + speaker.imageUrl).placeholder(R.drawable.ic_person).into(image)
     }
 }
