1d4e34d6f2a1e0ddc6a9017b833a82fb53bbe1d3
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-10-31 20:03:05 +0100

    Skip test till we decide how Firebase Speaker will look like

--- app/src/test/java/pl/droidcon/app/data/mapper/SpeakerMapperTest.kt
+++ app/src/test/java/pl/droidcon/app/data/mapper/SpeakerMapperTest.kt
@@ -3,9 +3,11 @@ package pl.droidcon.app.data.mapper
 import com.google.common.truth.Truth.assertThat
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import pl.droidcon.app.data.local.SpeakerLocal
+import pl.droidcon.app.data.network.FirebaseSpeaker
 import pl.droidcon.app.data.network.SpeakerRemote
 import pl.droidcon.app.domain.Speaker
 
@@ -20,16 +22,24 @@ class SpeakerMapperTest {
              fb: String, twitter: String, gh: String, linkedIn: String, gplus: String, image: String) {
 
         val speaker = Speaker(id = id, firstName = firstName, lastName = lastName, title = title, description = description, websiteUrl = websiteUrl, facebookUrl = fb, twitterUrl = twitter, githubUrl = gh, linkedinUrl = linkedIn, googlePlusUrl = gplus, imageUrl = image)
-        val speakerRemote = SpeakerRemote(id = id, firstName = firstName, lastName = lastName, title = title, description = description, websiteUrl = websiteUrl, facebookUrl = fb, twitterUrl = twitter, githubUrl = gh, linkedinUrl = linkedIn, googlePlusUrl = gplus, imageUrl = image)
         val speakerLocal = SpeakerLocal(id = id, firstName = firstName, lastName = lastName, title = title, description = description, websiteUrl = websiteUrl, facebookUrl = fb, twitterUrl = twitter, githubUrl = gh, linkedinUrl = linkedIn, googlePlusUrl = gplus, imageUrl = image)
 
-        val speakerFromRemote = systemUnderTest.map(speakerRemote)
         val speakerFromLocal = systemUnderTest.map(speakerLocal)
         val speakerLocalFromDomain = systemUnderTest.map(speaker)
 
-        assertThat(speakerFromRemote).isEqualTo(speaker)
         assertThat(speakerLocalFromDomain).isEqualTo(speakerLocal)
         assertThat(speakerFromLocal).isEqualTo(speaker)
+    }
+
+    @Test
+    @Ignore("will implement rest once we setup all firebase speaker attributes")
+    fun mapFirebase() {
+        val firebaseSpeakerRemote = FirebaseSpeaker(name = "first_name", surname = "last_name", photoUrl = "photo_url")
+//        val speaker = Speaker(firstName = "first_name", lastName = "last_name", imageUrl = "photo_url")
+//
+//        val speakerFromRemote = systemUnderTest.map(firebaseSpeakerRemote)
+//
+//        assertThat(speaker).isEqualTo(speakerFromRemote)
     }
 
     @Suppress("unused")
