b4e1450bd80a1667670d8a26c6641e301dc8ff3a
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-11-20 20:50:38 +0100

    separate layout for info bars

--- app/src/main/java/pl/droidcon/app/agenda/view/AgendaAdapter.kt
+++ app/src/main/java/pl/droidcon/app/agenda/view/AgendaAdapter.kt
@@ -5,50 +5,122 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 import pl.droidcon.app.R
 import pl.droidcon.app.domain.TalkPanel
 
-class AgendaAdapter(private val talks: List<TalkPanel>) : RecyclerView.Adapter<AgendaViewHolder>() {
+class AgendaAdapter(private val talks: List<TalkPanel>) : RecyclerView.Adapter<AgendaHolder>() {
 
     private val subject: PublishSubject<TalkPanel> = PublishSubject.create()
 
-    override fun onBindViewHolder(holder: AgendaViewHolder, position: Int) = holder.bindHolder(talks[position])
+    override fun onBindViewHolder(holder: AgendaHolder, position: Int) = holder.bindHolder(talks[position])
 
     override fun getItemCount() = talks.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AgendaViewHolder = AgendaViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.agenda_item, parent, false), subject
-    )
+    override fun getItemViewType(position: Int): Int = if (talks[position].sessionType == "singleSlot") 0 else 1
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AgendaHolder {
+        when (viewType) {
+            0 -> return AgendaSingleHolder(LayoutInflater.from(parent.context).inflate(R.layout.agenda_item, parent, false), subject)
+            else -> return AgendaTripleHolder(LayoutInflater.from(parent.context).inflate(R.layout.agenda_item_tripple, parent, false), subject)
+        }
+    }
 }
 
-class AgendaViewHolder(private val item: View,
-                       private val subject: Subject<TalkPanel>) : RecyclerView.ViewHolder(item) {
+class AgendaSingleHolder(private val item: View,
+                         private val subject: Subject<TalkPanel>) : AgendaHolder(item, subject) {
 
     private val startTime: TextView = item.findViewById(R.id.start_time)
     private val endTime: TextView = item.findViewById(R.id.end_time)
     private val title: TextView = item.findViewById(R.id.title)
-    private val description: TextView = item.findViewById(R.id.description)
 
     @SuppressLint("SetTextI18n")
-    fun bindHolder(talk: TalkPanel) {
+    override fun bindHolder(talk: TalkPanel) {
         val talk1 = talk.talks[0]
         val talk2 = talk.talks[1]
         val talk3 = talk.talks[2]
 
-        if (talk.sessionType == "singleSlot") {
-            title.text = talk2.title + talk1.title
-        }
-
-        if (talk.sessionType == "tripleSlot") {
-            title.text = talk1.speakers[0].firstName
-            description.text = talk2.speakers.firstOrNull()?.firstName + "     " + talk3.speakers.firstOrNull()?.firstName
-        }
+        title.text = talk2.title + talk1.title
 
         startTime.text = talk.start
         endTime.text = talk.end
     }
+}
+
+class AgendaTripleHolder(private val item: View,
+                         private val subject: Subject<TalkPanel>) : AgendaHolder(item, subject) {
+
+    private val startTime: TextView = item.findViewById(R.id.start_time)
+    private val endTime: TextView = item.findViewById(R.id.end_time)
+
+    private val speaker1picture: ImageView = item.findViewById(R.id.speaker_1_picture)
+    private val speaker1room: TextView = item.findViewById(R.id.speaker_1_room)
+    private val speaker2picture: ImageView = item.findViewById(R.id.speaker_2_picture)
+    private val speaker2room: TextView = item.findViewById(R.id.speaker_2_room)
+    private val speaker3picture: ImageView = item.findViewById(R.id.speaker_3_picture)
+    private val speaker3room: TextView = item.findViewById(R.id.speaker_3_room)
+
+    @SuppressLint("SetTextI18n")
+    override fun bindHolder(talk: TalkPanel) {
+        val talk1 = talk.talks[0]
+        val talk2 = talk.talks[1]
+        val talk3 = talk.talks[2]
+
+        startTime.text = talk.start
+        endTime.text = talk.end
+
+        Picasso
+                .with(item.context)
+                .load(talk1.speakers.firstOrNull()?.imageUrl)
+                .placeholder(R.drawable.ic_person)
+                .error(R.drawable.ic_sad)
+                .into(speaker1picture)
+
+        if (talk2.speakers.isEmpty() && talk3.speakers.isEmpty()) {
+            speaker2picture.visibility = View.GONE
+            speaker3picture.visibility = View.GONE
+
+            speaker2room.visibility = View.GONE
+            speaker3room.visibility = View.GONE
+        } else {
+            speaker2picture.visibility = View.VISIBLE
+            speaker3picture.visibility = View.VISIBLE
+
+            speaker2room.visibility = View.VISIBLE
+            speaker3room.visibility = View.VISIBLE
+
+            if (talk2.speakers.isEmpty()) {
+                speaker2picture.setImageResource(R.drawable.ic_sad)
+            } else
+                Picasso
+                        .with(item.context)
+                        .load(talk2.speakers.firstOrNull()?.imageUrl)
+                        .placeholder(R.drawable.ic_person)
+                        .error(R.drawable.ic_sad)
+                        .into(speaker2picture)
+
+
+            if (talk3.speakers.isEmpty()) {
+                speaker3picture.setImageResource(R.drawable.ic_sad)
+            } else
+                Picasso
+                        .with(item.context)
+                        .load(talk3.speakers.firstOrNull()?.imageUrl)
+                        .placeholder(R.drawable.ic_person)
+                        .error(R.drawable.ic_sad)
+                        .into(speaker3picture)
+        }
+
+
+    }
+}
+
+abstract class AgendaHolder(private val item: View,
+                            private val subject: Subject<TalkPanel>) : RecyclerView.ViewHolder(item) {
+
+    abstract fun bindHolder(talk: TalkPanel)
 }
