c6ddab999a2e0870432b668973cc9121a4e2d962
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-11-26 19:32:53 +0100

    Display second speaker for session if available

--- app/src/main/java/pl/droidcon/app/sessions/SessionActivity.kt
+++ app/src/main/java/pl/droidcon/app/sessions/SessionActivity.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.activity_session.*
 import pl.droidcon.app.R
 import pl.droidcon.app.domain.Session
+import pl.droidcon.app.domain.Speaker
 
 class SessionActivity : AppCompatActivity() {
 
@@ -17,15 +20,24 @@ class SessionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_session)
 
         val session = session(intent)
+        val firstSpeaker = session.speakers[0]
 
         session_title.text = session.sessionTitle
         session_description.text = session.sessionDescription
-        Picasso.with(this).load(session.speakers.first().imageUrl).into(session_picture)
+        Picasso.with(this).load(firstSpeaker.imageUrl).into(session_picture)
 
-        val speaker = session.speakers[0]
-        session_speaker_1_name.text = "${speaker.firstName} ${speaker.lastName}"
-        session_speaker_1_title.text = speaker.title
-        Picasso.with(this).load(session.speakers.first().imageUrl).transform(CropCircleTransformation()).into(session_speaker_1_picture)
+        setupSpeaker(firstSpeaker, session_speaker_1_name, session_speaker_1_title, session_speaker_1_picture)
+
+        if (session.speakers.size == 2) {
+            setupSpeaker(session.speakers[1], session_speaker_2_name, session_speaker_2_title, session_speaker_2_picture)
+        }
+
+    }
+
+    private fun setupSpeaker(speaker: Speaker, nameTextView: TextView, titleTextView: TextView, speakerImageView: ImageView) {
+        nameTextView.text = "${speaker.firstName} ${speaker.lastName}"
+        titleTextView.text = speaker.title
+        Picasso.with(this).load(speaker.imageUrl).transform(CropCircleTransformation()).into(speakerImageView)
     }
 
     companion object {
