2bcf8633dffb2744c86caa5bc4124ad57c89ddcb
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-11-28 09:00:46 +0100

    Padding agenda items to fragment using DayID

--- app/src/main/java/pl/droidcon/app/agenda/AgendaItemPresenter.kt
+++ app/src/main/java/pl/droidcon/app/agenda/AgendaItemPresenter.kt
@@ -1,15 +1,36 @@
 package pl.droidcon.app.agenda
 
 import android.widget.ImageView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
+import pl.droidcon.app.agenda.interactor.AgendaRepository
+import pl.droidcon.app.domain.Agenda
 import pl.droidcon.app.domain.Session
 import javax.inject.Inject
 
-class AgendaItemPresenter @Inject constructor() {
+class AgendaItemPresenter @Inject constructor(private val agendaRepository: AgendaRepository) {
 
     private var view: AgendaItemView? = null
 
-    fun attachView(agendaItemView: AgendaItemView?) {
+    private val disposables = CompositeDisposable()
+
+    fun attachView(agendaItemView: AgendaItemView?, dayId: Int) {
         this.view = agendaItemView
+
+        if (view == null) {
+            disposables.clear()
+        } else {
+            agendaRepository.get()
+                    .subscribeOn(Schedulers.io())
+                    .distinctUntilChanged()
+                    .filter { agenda: Agenda -> agenda.days.isNotEmpty() }
+                    .map { agenda -> agenda.days[dayId].talkPanels }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ view?.display(it) }, { it.printStackTrace() })
+                    .addTo(disposables)
+        }
     }
 
     fun openSession(speakerPicture: ImageView, session: Session) {
--- app/src/main/java/pl/droidcon/app/agenda/AgendaItemView.kt
+++ app/src/main/java/pl/droidcon/app/agenda/AgendaItemView.kt
@@ -2,8 +2,11 @@ package pl.droidcon.app.agenda
 
 import android.widget.ImageView
 import pl.droidcon.app.domain.Session
+import pl.droidcon.app.domain.TalkPanel
 
 interface AgendaItemView {
+    fun display(talkPanels: List<TalkPanel>)
+
     fun openSession(speakerPicture: ImageView, session: Session)
 
 }
