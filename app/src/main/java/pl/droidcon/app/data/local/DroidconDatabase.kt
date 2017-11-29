b968dde480fc2115c0859cd50c8cd796c62edb82
whiter13 <bialkowski.jakub@gmail.com>
2017-11-29 08:37:52 +0100

    Increase database version after changes to SQL models.

--- app/src/main/java/pl/droidcon/app/data/local/DroidconDatabase.kt
+++ app/src/main/java/pl/droidcon/app/data/local/DroidconDatabase.kt
@@ -11,7 +11,7 @@ arrayOf(
         DayLocal::class,
         TalkPanelLocal::class,
         TalkLocal::class),
-        version = 3)
+        version = 4)
 @TypeConverters(Converters::class)
 abstract class DroidconDatabase : RoomDatabase() {
 
