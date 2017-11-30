20e2141fe9dbd54f4040c2631680b245d32255dd
Przemek Jakubczyk <pjakubczyk@applause.com>
2017-11-30 16:08:31 +0100

    Remove logs

--- app/src/main/java/pl/droidcon/app/agenda/view/AgendaFragment.kt
+++ app/src/main/java/pl/droidcon/app/agenda/view/AgendaFragment.kt
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -103,8 +102,6 @@ class AgendaItemFragment : Fragment(), AgendaItemView {
     }
 
     override fun display(talkPanels: List<TalkPanel>) {
-        val size = talkPanels.size
-        Log.d("DDD", this.toString() + " " + size)
         view?.let {
             adapter.add(talkPanels)
             agendaView.layoutManager = LinearLayoutManager(agendaView.context)
